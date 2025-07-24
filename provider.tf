35e5eb6d7fe0fc2faaf3b41ee9cf917c58a78cd8
Avanish Kumar Mishra <avanish.mishra011@gmail.com>
2025-07-24 22:06:14 +0530

    kahani likhi maine ek chhoti si

--- provider.tf
+++ provider.tf
@@ -11,3 +11,5 @@ provider "azurerm" {
   features {}
   subscription_id = "8c84403a-3405-4e76-b4b7-c5eb9767a3e0"
 }
+# This provider configuration sets up the AzureRM provider for Terraform.
+# It specifies the required provider version and the Azure subscription ID to be used.  
